cf5a550c271f1ad7a286b7f4fef671dff0133ce3
Christian <kalonjic34@gmail.com>
2024-08-16 11:04:50 +0200

    Workbook 2.2 coding exercise

---
+++ 02-Variables/src/workbook/Biography.java
@@ -0,0 +1,18 @@
+package io.github.jiangdequan;
+
+public class Biography {
+    public static void main(String[] args) {
+        String name = "Chris";
+        int age = 24;
+        String country = "France";
+        String sport = "Basketball";
+        int hours = 2;
+        String game = "Chess";
+        String subject = "English";
+        char grade = 'B';
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favorite sport is " + sport + ". I play for " + hours + "(s) a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ". I scored a " + grade + ".");
+    }
+}
