503062b3f33ef5cbaaf14e0d128876e2b64fcdb9
Christian <kalonjic34@gmail.com>
2024-08-16 10:50:20 +0200

    Strings and char in java

---
+++ 02-Variables/src/section2/Grades.java
@@ -0,0 +1,13 @@
+package io.github.jiangdequan;
+
+public class Grades {
+    public static void main(String[] args) {
+        char potions = 'A';
+        char arithemamcy = 'B';
+        char charms = 'C';
+
+        System.out.println("Potions " + potions);
+        System.out.println("Arithemamcy " + arithemamcy);
+        System.out.println("Charms " + charms);
+    }
+}
