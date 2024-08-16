b7bd2220883d45fa63f3d1838e8a3f19a55754a2
Christian <kalonjic34@gmail.com>
2024-08-16 10:07:43 +0200

    Variables introduction

---
+++ 02-Variables/src/section2/Bus.java
@@ -0,0 +1,13 @@
+package io.github.jiangdequan;
+
+public class Bus {
+    public static void main(String[] args) {
+        int passenger = 0;
+        passenger += 9;
+        System.out.println(passenger);
+        passenger -= 5;
+        System.out.println(passenger);
+        passenger -= 4;
+        System.out.println(passenger);
+    }
+}
---
+++ 02-Variables/src/section2/Variables.java
@@ -0,0 +1,13 @@
+package io.github.jiangdequan;
+
+public class Variables {
+    public static void main(String[] args) {
+        int passenger = 5;
+        System.out.println(passenger);
+
+        int busTicket = passenger;
+        System.out.println(busTicket);
+
+        // int maxValue = 2147483648;
+    }
+}
