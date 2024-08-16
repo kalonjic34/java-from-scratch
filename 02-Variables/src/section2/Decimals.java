33bd5a4cf9373289a5ea7d569a82051eb07b4e93
Christian <kalonjic34@gmail.com>
2024-08-16 11:20:54 +0200

    Long and double data types

---
+++ 02-Variables/src/section2/Decimals.java
@@ -0,0 +1,12 @@
+package io.github.jiangdequan;
+
+public class Decimals {
+    public static void main(String[] args) {
+        double percentage = 73.5;
+        System.out.println(percentage + " of perentages are made up");
+
+        double wallet = 20; // always use double for math calculations
+        int people = 3;
+        System.out.println(wallet / people);
+    }
+}
---
+++ 02-Variables/src/section2/WholeNumbers.java
@@ -0,0 +1,12 @@
+package io.github.jiangdequan;
+
+public class WholeNumbers {
+    public static void main(String[] args) {
+
+        long globalPopulation = 8000000000l;
+        System.out.println("The globel population is " + globalPopulation);
+        long dailyGoogleSearches = 8500000000l;
+        System.out.println("Daily, there are " + dailyGoogleSearches + " google searches");
+
+    }
+}
