954053ff893263116ad910a28fbb576dfc4c5295
Christian <kalonjic34@gmail.com>
2024-08-16 09:38:23 +0200

    Getting started challenge

---
+++ 01-Getting-Started/src/YourInitials.java
@@ -0,0 +1,13 @@
+package io.github.jiangdequan;
+
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println(" CCC            K   K");
+        System.out.println("C    C          K  K");
+        System.out.println("C               K K");
+        System.out.println("C               KK");
+        System.out.println("C               K K");
+        System.out.println("C    C          K  K");
+        System.out.println(" CCC            K   K");
+    }
+}
