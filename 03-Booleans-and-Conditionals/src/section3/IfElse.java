124318274ef96a0b94ac434a8cce751d662d875b
Christian <kalonjic34@gmail.com>
2024-08-19 19:59:11 +0200

    If else statements

---
+++ 03-Booleans-and-Conditionals/src/section3/IfElse.java
@@ -0,0 +1,13 @@
+package io.github.jiangdequan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int grade = 50;
+
+        if (grade >= 50) {
+            System.out.println("Great job you passed!");
+        } else {
+            System.out.println("Study hard next time!");
+        }
+    }
+}
