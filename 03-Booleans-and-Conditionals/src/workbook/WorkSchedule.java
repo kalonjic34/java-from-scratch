5e03cfe9e8eebe2bb5c7aa575ed18b279a666ab3
Christian <kalonjic34@gmail.com>
2024-08-19 23:29:38 +0200

    Workbook 3.5 code exercise

--- 03-Booleans-and-Conditionals/src/workbook/WorkSchedule.java
+++ 03-Booleans-and-Conditionals/src/workbook/WorkSchedule.java
@@ -1,5 +1,16 @@
 package io.github.jiangdequan;
 
 public class WorkSchedule {
-    
+    public static void main(String[] args) {
+        int day = 6;
+        boolean holiday = false;
+
+        if (holiday) {
+            System.out.println("Woohoo, no work");
+        } else if (day == 6 || day == 7) {
+            System.out.println("It's the weekend, no work!");
+        } else if (day < 6 && day >= 1) {
+            System.out.println("Wake up at 7:00 :(");
+        }
+    }
 }
