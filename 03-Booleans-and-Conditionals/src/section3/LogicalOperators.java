4188ee60fc112d2438d033e34e727d9b931f4eda
Christian <kalonjic34@gmail.com>
2024-08-19 21:46:42 +0200

    Logical operators

---
+++ 03-Booleans-and-Conditionals/src/section3/LogicalOperators.java
@@ -0,0 +1,25 @@
+package io.github.jiangdequan;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int chemistryGrade = 67;
+        int englishGrade = 65;
+        String language = "Java";
+
+        if (chemistryGrade > 75 || englishGrade > 75 || language.equals("Java")) {
+            System.out.println("Congratulations, you got the scholarship");
+        } else {
+            System.out.println("We're sorry, you didnt get the scholarship");
+        }
+
+        int credits = 56;
+        double GPA = 1.2;
+
+        if (credits >= 40 && GPA >= 2.0) {
+            System.out.println("You earned your diploma!");
+        } else {
+            System.out.println("Sorry you need at least 40 credits and a minimum GPA of 2.0");
+        }
+
+    }
+}
