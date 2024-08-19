f8ac3aff005df0a7b5ab87a7c4dc595a3daadeba
Christian <kalonjic34@gmail.com>
2024-08-19 19:38:01 +0200

    Booleans and comparisons - introduction

---
+++ 03-Booleans-and-Conditionals/src/section3/Booleans.java
@@ -0,0 +1,11 @@
+package io.github.jiangdequan;
+
+public class Booleans {
+    public static void main(String[] args) {
+        boolean bool = true;
+        boolean bool2 = false;
+
+        System.out.println(bool);
+        System.out.println(bool2);
+    }
+}
