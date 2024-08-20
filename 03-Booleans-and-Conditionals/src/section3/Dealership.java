7c74de023e811c81af8fecf983f69b8aea67f759
Christian <kalonjic34@gmail.com>
2024-08-20 11:43:41 +0200

    Dealership project

---
+++ 03-Booleans-and-Conditionals/src/section3/Dealership.java
@@ -0,0 +1,25 @@
+package io.github.jiangdequan;
+
+import java.util.Scanner;
+
+public class Dealership {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the Java Dealership");
+        System.out.println("  .  Select option 'a' to buy a car");
+        System.out.println("  .  Select option 'b' to sell a car");
+        String option = scan.nextLine();
+        switch (option) {
+            case "a":
+                System.out.println("You choose option " + option);
+                break;
+            case "b":
+                System.out.println("You choose option " + option);
+                break;
+            default:
+                System.out.println("Invalid option");
+                break;
+        }
+        scan.close();
+    }
+}
