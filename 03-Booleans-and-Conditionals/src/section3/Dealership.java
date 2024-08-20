958afd3a5eecd0bfac5c590354a4ae8fedfd7757
Christian <kalonjic34@gmail.com>
2024-08-20 12:33:54 +0200

    Dealership project part 2

--- 03-Booleans-and-Conditionals/src/section3/Dealership.java
+++ 03-Booleans-and-Conditionals/src/section3/Dealership.java
@@ -11,7 +11,25 @@ public class Dealership {
         String option = scan.nextLine();
         switch (option) {
             case "a":
-                System.out.println("You choose option " + option);
+                System.out.println("What is your budget?");
+                int budget = scan.nextInt();
+                if (budget >= 10000) {
+                    System.out.println("Great! A Nissan Altima is available");
+                    System.out.println("\nDo you have insurance? Write yes or no");
+                    scan.nextLine();
+                    String insurance = scan.nextLine();
+                    System.out.println("\nDo you have a license? Write yes or no");
+                    String license = scan.nextLine();
+                    System.out.println("What is you credit score?");
+                    int creditScore = scan.nextInt();
+                    if (insurance.equals("yes") && license.equals("yes") && creditScore > 660) {
+                        System.out.println("Sold! Pleasure doing business with you!");
+                    } else {
+                        System.out.println("Not eligiable, We are sorry");
+                    }
+                } else {
+                    System.out.println("We dont sell cars under $10000. Sorry");
+                }
                 break;
             case "b":
                 System.out.println("You choose option " + option);
