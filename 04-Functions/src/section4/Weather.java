680ccaef897ae1ca99c404d480b8338cf71b12bf
Christian <kalonjic34@gmail.com>
2024-08-28 13:12:08 +0200

    Workbook 4.3 code exercise

---
+++ 04-Functions/src/section4/Weather.java
@@ -0,0 +1,20 @@
+package io.github.jiangdequan;
+
+public class Weather {
+    public static void main(String[] args) {
+        printTempretures(77);
+        printTempretures(61);
+        printTempretures(55);
+
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        return celsius;
+    }
+
+    public static void printTempretures(double fahrenheit) {
+        System.out.println("F: " + fahrenheit);
+        System.out.println("C: " + fahrenheitToCelsius(fahrenheit) + "\n");
+    }
+}
