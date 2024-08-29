f1ff57d6808d650cc52bf58705affeeea3ed78e4
Christian <kalonjic34@gmail.com>
2024-08-29 11:35:20 +0200

    Scope

---
+++ 04-Functions/src/section4/Scope.java
@@ -0,0 +1,15 @@
+package section4;
+
+public class Scope {
+    static int dogs = 5;
+
+    public static void main(String[] args) {
+        System.out.println(dogs);
+        someFunction();
+    }
+
+    public static void someFunction() {
+        System.out.println(dogs);
+    }
+
+}
