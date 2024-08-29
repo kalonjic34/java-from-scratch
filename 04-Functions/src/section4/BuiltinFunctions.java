789fc5b74b1bf1817c1417d3facb3a39fd027d77
Christian <kalonjic34@gmail.com>
2024-08-29 11:45:55 +0200

    Builtin functions

---
+++ 04-Functions/src/section4/BuiltinFunctions.java
@@ -0,0 +1,13 @@
+package section4;
+
+public class BuiltinFunctions {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        double sine = Math.sin(1.2);
+        System.out.println("The sine of 1.2 is: " + sine);
+
+        double power = Math.pow(2, 4);
+        System.out.println("2 to the power 4 is: " + power);
+    }
+}
