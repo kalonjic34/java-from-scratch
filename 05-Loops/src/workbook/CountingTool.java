b47fff53742e983e883f34b4e86cd982fcb9eb99
Christian <kalonjic34@gmail.com>
2024-08-31 16:06:29 +0200

    Workbook 5.3 revisited

--- 05-Loops/src/workbook/CountingTool.java
+++ 05-Loops/src/workbook/CountingTool.java
@@ -5,10 +5,23 @@ import java.util.Scanner;
 public class CountingTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Hi, Timmy! Choose a number to count to: ");
-        int number = scan.nextInt();
+        System.out.println("I hear you like to count by threes\n");
+        System.out.println("Jimmy: It depends.");
+        System.out.println("Oh, ok\n");
+
+        System.out.print("1. Pick a number to count by: ");
+        int countBy = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("2. Pick a number to start counting from: ");
+        int countFrom = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("3. Pick a number to count to: ");
+        int countTo = scan.nextInt();
+
         scan.close();
-        for (int i = 0; i <= number; i++) {
+        for (int i = countFrom; i <= countTo; i += countBy) {
             System.out.print(i + " ");
         }
     }
