e7f8a1980d42bb616c5af39e744b99847866bcbb
Christian <kalonjic34@gmail.com>
2024-08-31 14:55:48 +0200

    Workbook 5.3 & 5.4 code exercise

---
+++ 05-Loops/src/workbook/CountingTool.java
@@ -0,0 +1,15 @@
+package workbook;
+
+import java.util.Scanner;
+
+public class CountingTool {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Hi, Timmy! Choose a number to count to: ");
+        int number = scan.nextInt();
+        scan.close();
+        for (int i = 0; i <= number; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
