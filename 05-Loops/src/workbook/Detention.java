be08ef573251390fae07b058fed84d06af15c66e
Christian <kalonjic34@gmail.com>
2024-08-31 14:37:56 +0200

    Added scanner to the code

--- 05-Loops/src/workbook/Detention.java
+++ 05-Loops/src/workbook/Detention.java
@@ -1,12 +1,16 @@
 package workbook;
 
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Hi Bart. I can write lines for you.");
         System.out.println("What do you want me to write?\n");
+        String writeLines = scan.nextLine();
 
         for (int i = 1; i <= 99; i++) {
-            System.out.println("I will not forget semi-colons");
+            System.out.println(writeLines);
         }
     }
 }
