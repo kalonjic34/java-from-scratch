0c069aa332c0b9e667628f7c123db8c206dbd441
Christian <kalonjic34@gmail.com>
2024-08-31 14:33:30 +0200

    Worbook 51 & 52 code exercises

---
+++ 05-Loops/src/workbook/Detention.java
@@ -0,0 +1,12 @@
+package workbook;
+
+public class Detention {
+    public static void main(String[] args) {
+        System.out.println("Hi Bart. I can write lines for you.");
+        System.out.println("What do you want me to write?\n");
+
+        for (int i = 1; i <= 99; i++) {
+            System.out.println("I will not forget semi-colons");
+        }
+    }
+}
