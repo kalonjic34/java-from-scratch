3c518425aa49fb1d55d4fa774b4b0ead0ed0c3ee
Christian <kalonjic34@gmail.com>
2024-09-03 14:31:54 +0200

    Workbook 6.1 code exercise

---
+++ 06-Arrays/src/workbook/Exam.java
@@ -0,0 +1,16 @@
+package workbook;
+
+public class Exam {
+    public static void main(String[] args) {
+        String[] seats = { "Harry", "Neville", "Ron", "Hermione", "Seamus" };
+
+        System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
+
+        System.out.println(seats[0] + ", you will take seat " + 0);
+        System.out.println(seats[1] + ", you wlll take seat " + 1);
+        System.out.println(seats[2] + ", you wlll take seat " + 2);
+        System.out.println(seats[3] + ", you wlll take seat " + 3);
+        System.out.println(seats[4] + ", you wlll take seat " + 4);
+
+    }
+}
