6064bd90ff2b232bfbd7362d45d7ff2abfbfa9b4
Christian <kalonjic34@gmail.com>
2024-09-07 16:55:38 +0200

    Looping 2d arrays

--- 06-Arrays/src/section6/TDarrays.java
+++ 06-Arrays/src/section6/TDarrays.java
@@ -1,7 +1,5 @@
 package section6;
 
-import java.util.Arrays;
-
 public class TDarrays {
     public static void main(String[] args) {
         int[][] grades = {
@@ -9,9 +7,23 @@ public class TDarrays {
                 { 76, 65, 64, 67 },
                 { 95, 98, 99, 100 }
         };
+        for (int i = 0; i < grades.length; i++) {
+            switch (i) {
+                case 0:
+                    System.out.print("Harry: ");
+                    break;
+                case 1:
+                    System.out.print("Ron: ");
+                    break;
+                case 2:
+                    System.out.print("Hermione: ");
+                    break;
+            }
+            for (int j = 0; j < grades[i].length; j++) {
+                System.out.print(grades[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
 
-        System.out.println("\tHarry: " + Arrays.toString(grades[0]));
-        System.out.println("\tRon: " + Arrays.toString(grades[1]));
-        System.out.println("\tHermione: " + Arrays.toString(grades[2]));
     }
 }
