f65760cf8e2d0a2c9861526cb06c23008c819a48
Christian <kalonjic34@gmail.com>
2024-09-09 14:19:40 +0200

    Constructors

--- 07-The-World-of-Objects/src/section7/Main.java
+++ 07-The-World-of-Objects/src/section7/Main.java
@@ -2,17 +2,8 @@ package section7;
 
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car();
-        nissan.make = "Nissan";
-        nissan.price = 10000;
-        nissan.year = 2020;
-        nissan.color = "Green";
-
-        Car dodge = new Car();
-        dodge.make = "Dodge";
-        dodge.price = 11000;
-        dodge.year = 2019;
-        dodge.color = "Blue";
+        Car nissan = new Car("Nissan", 10000, 2020, "Green");
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
 
         System.out.println("This " + nissan.make + " is worth $" + nissan.price + ". It was built in " + nissan.year
                 + ". It is " + nissan.color + "\n");
