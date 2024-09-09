8a5d7ff69f9d7c98e302266ed9fa492690da081e
Christian <kalonjic34@gmail.com>
2024-09-09 14:50:09 +0200

    Workbook 7.2 code exercise

--- 07-The-World-of-Objects/src/workbook/Main.java
+++ 07-The-World-of-Objects/src/workbook/Main.java
@@ -4,21 +4,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Person chris = new Person();
-        chris.name = "Chris";
-        chris.nationality = "Dutch";
-        chris.dateOfBirth = "04/15/2000";
-        chris.passport = new String[] {
-                chris.name,
-                chris.nationality,
-                chris.dateOfBirth,
-        };
-        chris.seatNumber = 8;
+        Person person = new Person("Chris", "Dutch", "04/15/2000", 8);
 
-        System.out.println(chris.name);
-        System.out.println(chris.nationality);
-        System.out.println(chris.dateOfBirth);
-        System.out.println(Arrays.toString(chris.passport));
-        System.out.println(chris.seatNumber);
+        System.out.println("Name: " + person.name);
+        System.out.println("Nationality: " + person.nationality);
+        System.out.println("Date of Birth: " + person.dateOfBirth);
+        System.out.println("Seat Number: " + person.seatNumber);
+
     }
 }
