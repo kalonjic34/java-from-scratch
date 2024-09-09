2e1bab7015567d46a0017a9b3dbbd477ce22c479
Christian <kalonjic34@gmail.com>
2024-09-09 15:09:44 +0200

    Setters

--- 07-The-World-of-Objects/src/section7/Car.java
+++ 07-The-World-of-Objects/src/section7/Car.java
@@ -17,15 +17,31 @@ public class Car {
         return make;
     }
 
+    public void setMake(String make) {
+        this.make = make;
+    }
+
     public double getPrice() {
         return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public int getYear() {
         return year;
     }
 
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     public String getColor() {
         return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
--- 07-The-World-of-Objects/src/section7/Main.java
+++ 07-The-World-of-Objects/src/section7/Main.java
@@ -1,16 +1,22 @@
 package section7;
 
 public class Main {
-    public static void main(String[] args) {
-        Car nissan = new Car("Nissan", 10000, 2020, "Green");
-        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
+        public static void main(String[] args) {
+                Car nissan = new Car("Nissan", 10000, 2020, "Green");
+                Car dodge = new Car("Dodge", 11000, 2019, "Blue");
 
-        System.out
-                .println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + ". It was built in "
-                        + nissan.getYear()
-                        + ". It is " + nissan.getColor() + "\n");
-        System.out.println(
-                "This " + dodge.getMake() + " is worth $" + dodge.getPrice() + ". It was built in " + dodge.getYear()
-                        + ". It is " + dodge.getColor() + "\n");
-    }
+                nissan.setColor("Jet Black");
+                dodge.setColor("Jet Black");
+
+                nissan.setPrice(nissan.getPrice() / 2);
+                dodge.setPrice(dodge.getPrice() / 2);
+
+                System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice()
+                                + ". It was built in "
+                                + nissan.getYear()
+                                + ". It is " + nissan.getColor() + "\n");
+                System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + ". It was built in "
+                                + dodge.getYear()
+                                + ". It is " + dodge.getColor() + "\n");
+        }
 }
