f22f38ee48f3c169baf28c3c6bd15837682b83b7
Christian <kalonjic34@gmail.com>
2024-09-09 15:56:29 +0200

    The drive task

--- 07-The-World-of-Objects/src/section7/Main.java
+++ 07-The-World-of-Objects/src/section7/Main.java
@@ -3,19 +3,10 @@ package section7;
 public class Main {
         public static void main(String[] args) {
                 Car nissan = new Car("Nissan", 10000, 2020, "Green");
-
+                Car dodge = new Car("Dodge", 11000, 2019, "Blue");
                 Car nissan2 = new Car(nissan);
-
                 nissan2.setColor("Yellow");
-                nissan.setColor("Orange");
-                nissan2.setColor("Blue");
-                nissan.setColor("Purple");
-                nissan2.setColor("Fushsia");
-                nissan.setColor("Beige");
 
-                System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice()
-                                + ". It was built in "
-                                + nissan.getYear()
-                                + ". It is " + nissan.getColor() + "\n");
+                nissan2.drive();
         }
 }
