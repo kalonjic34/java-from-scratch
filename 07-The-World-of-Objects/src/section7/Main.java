0724795b1128dc2e447dc19e5aa330351a295e69
Christian <kalonjic34@gmail.com>
2024-09-09 14:57:30 +0200

    Getters

--- 07-The-World-of-Objects/src/section7/Main.java
+++ 07-The-World-of-Objects/src/section7/Main.java
@@ -5,9 +5,12 @@ public class Main {
         Car nissan = new Car("Nissan", 10000, 2020, "Green");
         Car dodge = new Car("Dodge", 11000, 2019, "Blue");
 
-        System.out.println("This " + nissan.make + " is worth $" + nissan.price + ". It was built in " + nissan.year
-                + ". It is " + nissan.color + "\n");
-        System.out.println("This " + dodge.make + " is worth $" + dodge.price + ". It was built in " + dodge.year
-                + ". It is " + dodge.color + "\n");
+        System.out
+                .println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + ". It was built in "
+                        + nissan.getYear()
+                        + ". It is " + nissan.getColor() + "\n");
+        System.out.println(
+                "This " + dodge.getMake() + " is worth $" + dodge.getPrice() + ". It was built in " + dodge.getYear()
+                        + ". It is " + dodge.getColor() + "\n");
     }
 }
